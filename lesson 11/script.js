79ea19ddc80e734dee22410ff7ee9e4324371beb
Bogdan <bogdemona@mail.ru>
2021-05-02 18:19:51 +0300

    create: learning

---
+++ lesson 11/script.js
@@ -0,0 +1,19 @@
+// Упорядочивание данных
+// Массивы array
+// Объекты object
+/*
+const mon = "Понедельник";
+const tue = "Вторник";
+const wed = "Среда";
+const thu = "Четверг";
+const fri = "Пятница";
+
+console.log(mon,tue,wed,thu,fri)
+
+*/
+
+const bogDan = "DAYOFSEX";
+
+const daysOfWeek = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье", "DAYOFSEX" ];
+
+console.log(daysOfWeek[7])
---
+++ lesson 9/script.js
@@ -0,0 +1,18 @@
+// ТИПЫ ДАННЫХ строка
+
+const name = "Bogdan";
+
+
+// Логическое выражение 
+
+const isMarried = false;
+
+// Целое число
+
+const a = 222;
+
+// Дробное число 
+
+const height = 1.75;
+
+console.log(name,isMarried,height)
