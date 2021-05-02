b08f639e760ce5e760f07002596ebe6ac025e802
Bogdan <bogdemona@mail.ru>
2021-05-03 00:11:06 +0300

    create: add 13 lesson

--- lesson 13 (2.2)/script.js
+++ lesson 13 (2.2)/script.js
@@ -1,31 +1,50 @@
-// {} --- Объект, [] --- Массив
-const info = {
-	name: "Bogdan",
-	age: "28",
-	city: "Moskow",
-	isMarried: true,
-	favMovies: ["Гарри Поттер", "Матрица", "Терминатор"],
-	favFood: 
-	[
-		{
-			name: "Cуп",
-			fatty: false
-		},
-		{
-			name: "Винегрет",
-			fatty: false
-		},
-		{
-			name: "Чизбургер",
-			fatty: false
-		},
+// // МОЯ ПЕРВАЯ ФУНКЦИЯ v1.1
 
-	]
+// function sayHello(name, age) {
+// 	return `Привет, ${name}!Я,JS!Твой возраст - ${age} лет`;
+// }
+// const hiBro = sayHello('Bogdan' , 28);
 
-};
-console.log(info.favMovies[2]);
+// console.log(hiBro)
 
 
+// Калькулятор на минус
+// const calculator = {
+// 	minus: function(a,b){
+// 		return a - b ;
+// 	}
+// }
+
+// const summa = calculator.minus(5,10);
+// console.log(summa);
+
+
+// // Калькулятор на плюс 
+// const calculator = {
+// 	plus: function (a, b) {
+// 		return a + b;
+// 	}
+// }
+// const summa = calculator.plus(5, 10);
+// console.log(summa);
 
 
 
+// const calculator = {
+// 	multi: function(a, b) {
+// 		return a * b;
+// 	}
+// }
+
+// const summa = calculator.multi(7,9);
+// console.log(summa);
+
+
+const calculator = {
+	division: function(a, b){
+		return a / b;
+	}
+}
+
+const summa = calculator.division(999, 3);
+console.log(summa);
